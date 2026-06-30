a85b1c48253dc131d706c5c9d13a48c5c04fd028
NguyenTruongGiang <23130082@st.hcmuaf.edu.vn>
2026-06-30 12:28:33 +0700

    fix

--- AppTheMuse/app/src/main/java/com/example/appthemuse/ui/viewmodel/AddChapterViewModel.kt
+++ AppTheMuse/app/src/main/java/com/example/appthemuse/ui/viewmodel/AddChapterViewModel.kt
@@ -8,7 +8,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
-//////
+
 data class AddChapterUiState(
     val title: String = "",
     val content: String = "",
